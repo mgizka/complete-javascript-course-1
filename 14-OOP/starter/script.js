c9c02d359e7e2f6fa6f921eed55fc24fc91167c5
Marcin Gizka <marcin.gizka@.com>
2021-11-26 16:38:14 +0100

    OOP chalange 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,23 @@
-'use strict';
+'use strict'
+
+const Car = function(make, speed){
+    this.make=make
+    this.speed=speed
+}
+
+Car.prototype.accelerate = function (){
+    this.speed+=10
+}
+
+Car.prototype.break = function(){
+    this.speed-=5
+}
+
+const car1 = new Car('BMW', 120), car2 = new Car('Mercedes',95)
+
+console.log(car1, car2)
+
+car1.accelerate();
+car2.break();
+
+console.log(car1, car2)
