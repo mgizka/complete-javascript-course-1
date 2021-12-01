4c3d9958d6c75463f6f032d0fbfea973afc29013
Marcin Gizka <mgizka@gmail.com>
2021-12-01 22:28:10 +0100

    blob to window

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (str) {
+  this.str = str;
+};
+
+const Student = function (str) {
+  Person.call(this, str);
+};
+
+const obj = new Student('str');
+console.log(obj);
+console.log(obj.str);
+console.log(obj.__proto__ === Person.prototype);
+console.log(obj.__proto__.hasOwnProperty('str'));
---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function fetchDocument(doc) {
+  fetch(doc)
+    .then(response => {
+      console.log(response);
+      return response.blob();
+    })
+    .then(blob => URL.createObjectURL(blob))
+    .then(function (data) {
+      console.log(data);
+      window.open(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+const doc =
+  'https://myopensearchbucket.s3.eu-central-1.amazonaws.com/TMF621_Trouble_Ticket_API_REST_Specification_R19.0.1.docx';
+fetchDocument(doc);
