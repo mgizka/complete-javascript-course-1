31f99e7524c84cdfaaf93df7519c9982f16615d3
Marcin Gizka <marcin.gizka@.com>
2021-12-04 01:08:23 +0100

    wer

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -16,14 +16,72 @@ console.log(userObj1, userObj2)
 userObj1.f(1,2,3,4)
 
 
-// Value of webAddress: 
-const webAddress = 'https://www.cosmicsolutionswebportal.com:8080/users?year=2020&type=analyst'
-const url = new URL(webAddress); 
-// Add code here
-url.search='year=2019'
+class AAA {
+    xx = 'AAA'
 
-console.log(webAddress)
+    aa(){
+        console.log(this, this.xx)
+        bb()
+    }
+
+    bb(){
+        console.log(this, this.xx)
+    }
+
+    cc = ()=>{
+        console.log(this, this.xx)
+        this.dd()
+    }
+
+    dd = ()=>{
+        console.log(this, this.xx)
+    }
+}
+
+function bb(){
+    console.log('bb')
+}
+
+const a = new AAA()
+
+console.log(`functions and functions expressions`)
+a.aa()
+a.bb()
+console.log(`arrow functions`)
+a.cc()
+a.dd()
 
 
+
+const countries =  [
+    {
+        cname : 'Portugal',
+        ccode : 'PO'
+    },{
+        cname : 'Poland',
+        ccode : 'PL'
+    }
+]
+
+const [cname]=countries
+
+const [portugal, poland] = countries
+
+console.log(countries, cname)
+console.log(typeof portugal)
+console.log(typeof poland)
+
+const obj1 = {
+    cname : 'Portugal',
+    ccode : 'PO',
+    ctest1 : 'A'  
+}, obj2 = {
+    cname : 'Poland',
+    ccode : 'PL',
+    ctest2 : 'B'
+}
+
+const o = {...obj1, ...obj2 }
+console.log(o)
 
 
