70611903204d2a26e72cc5c7437e5b580194222f
Marcin Gizka <marcin.gizka@.com>
2021-12-04 09:05:46 +0100

    next version img

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,35 +1,35 @@
 'use strict'
 
 
-async function whereAmI (lat, lng){
+async function whereAmI(lat, lng) {
     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then( (r) => { 
+        .then((r) => {
 
-        if(!r.ok)
-            throw new Error(`Cannot find data at this location ${r.status}`)
+            if (!r.ok)
+                throw new Error(`Cannot find data at this location ${r.status}`)
 
-        return r.json()
-    } )
-    .then( ({city,country}) => { 
+            return r.json()
+        })
+        .then(({ city, country }) => {
 
-        if(!city)
-            throw new Error(`Cannot find data at this location`)
+            if (!city)
+                throw new Error(`Cannot find data at this location`)
 
-        console.log(city, country) 
-    })
-    .catch( (e)=>{
-        console.error(e);
-    } )
+            console.log(city, country)
+        })
+        .catch((e) => {
+            console.error(e);
+        })
 }
-const res = whereAmI(52.508,32.381);
+const res = whereAmI(52.508, 32.381);
 
 
-function createImage(url){
-    return new Promise( function( resolve, reject ) {
-        console.log('fetching image '+ url)
+function createImage(url) {
+    return new Promise(function (resolve, reject) {
+        console.log('fetching image ' + url)
         let img = new Image()
 
-        img.addEventListener('load', resolve(img) )
+        img.addEventListener('load', resolve(img))
         img.addEventListener('error', reject(img))
 
         img.src = url;
@@ -37,22 +37,32 @@ function createImage(url){
 }
 
 
-const imgPath = 'https://elingwista.com/wp-content/uploads/2021/08/iStock-1070519460-1024x569.jpg'
+let images = document.querySelector('.images')
 
-createImage(imgPath).then(
-     (result)=>{
-        const images = document.querySelector('.images')
-        images.appendChild(result)
+createImage('https://elingwista.com/wp-content/uploads/2021/08/iStock-1070519460-1024x569.jpg')
+    .then(
+        (result) => {
+            images.replaceChildren(result)
+            return wait(2000)
+        }
+    )
+    .then(() => createImage('https://image.shutterstock.com/z/stock-photo-liquid-characters-d-render-1061737175.jpg')
+    ).then(
+        (result) => {
+            images.replaceChildren(result)
+            return wait(2000)
+        }
+    )
+    .catch(
+        e => console.log(e.message, e.stack)
+    )
+    .finally(
+        () => { images.style.display = 'none' }
+    )
 
-        wait(2000).then( () => { images.style.display = 'none' } )
-    }
-).catch( 
-    e => console.log('nok')
-)
-
-function wait( timeout ){
-    return new Promise( (resolve) => setTimeout( 
-        resolve, 
+function wait(timeout) {
+    return new Promise((resolve) => setTimeout(
+        resolve,
         timeout)
     )
 }
